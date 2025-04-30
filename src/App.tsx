b087a1c312dfdd35ff4d5ea67a8bddb0ef63b240
Roman Vashchenko <roman1991dp@gmail.com>
2025-05-01 00:09:59 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div>VocabBuilder</div>;
+  return (
+    <div>
+      <h1>VocabBuilder</h1>
+
+      <svg width={20} height={20}>
+        <use href="/src/assets/img/icons.svg#icon-ukraine"></use>
+      </svg>
+
+      <svg width={20} height={20}>
+        <use href="/src/assets/img/icons.svg#icon-united-kingdom"></use>
+      </svg>
+    </div>
+  );
 }
 
 export default App;
