4d32586dd3413ccd2272fb74f14fe26761f926a3
Roman Vashchenko <roman1991dp@gmail.com>
2025-07-29 22:21:32 +0300

    update

--- src/components/AddWordModal/AddWordModal.tsx
+++ src/components/AddWordModal/AddWordModal.tsx
@@ -12,27 +12,25 @@ interface addModalProps {
 
 const AddWordModal: FC<addModalProps> = ({ isOpen, onClose }) => {
   return (
-    <div>
-      <ReactModal
-        isOpen={isOpen}
-        onRequestClose={onClose}
-        className={css.modal}
-        overlayClassName={css.overlay}
-        bodyOpenClassName={css.bodyOpen}
-      >
-        <button type="button" className={css.closeBtn} onClick={onClose}>
-          <svg width={24} height={24}>
-            <use href="/src/assets/img/icons.svg#icon-close"></use>
-          </svg>
-        </button>
-        <p className={css.nameModal}>Add word</p>
-        <p className={css.textModal}>
-          Adding a new word to the dictionary is an important step in enriching
-          the language base and expanding the vocabulary.
-        </p>
-        <AddWordForm onClose={onClose} />
-      </ReactModal>
-    </div>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className={css.modal}
+      overlayClassName={css.overlay}
+      bodyOpenClassName={css.bodyOpen}
+    >
+      <button type="button" className={css.closeBtn} onClick={onClose}>
+        <svg width={24} height={24}>
+          <use href="/src/assets/img/icons.svg#icon-close"></use>
+        </svg>
+      </button>
+      <p className={css.nameModal}>Add word</p>
+      <p className={css.textModal}>
+        Adding a new word to the dictionary is an important step in enriching
+        the language base and expanding the vocabulary.
+      </p>
+      <AddWordForm onClose={onClose} />
+    </ReactModal>
   );
 };
 
